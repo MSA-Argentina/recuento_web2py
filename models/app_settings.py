e194c3a2796969c150680c75a31340d4c929a903
Mariano Reingart <reingart@gmail.com>
2013-08-04 02:42:35 -0300

    ajusto titulo para elecciones 2007

--- models/app_settings.py
+++ models/app_settings.py
@@ -3,7 +3,8 @@
 # Configuraciones globales:
 
 TITULO = "Recuento" 
-SUBTITULO = "Elecciones Provinciales - Provincia - Fecha"
+SUBTITULO = "Elecciones Nacionales - Republica Argentina - 28 de Octubre 2007"
 
 # cadena de conexion a base principal:
 PG_CNNSTR = "postgres://reingart:clave@localhost/recuento"
+
